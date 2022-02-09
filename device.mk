cc0b6b9458dd446033a449c9201fb32173e43498
Sushrut1101 <guptasushrut@gmail.com>
2022-02-09 17:23:51 +0530

    Nord: Try to Fix Decryption
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -67,6 +67,23 @@ TW_DEFAULT_BRIGHTNESS := 420
 TWRP_INCLUDE_LOGCAT := true
 TARGET_USES_LOGD := true
 
+# Additional Libraries
+TARGET_RECOVERY_DEVICE_MODULES += \
+	libion \
+	libicuc \
+	libandroidicu \
+	vendor.display.config@1.0 \
+	vendor.display.config@2.0
+
+RECOVERY_LIBRARY_SOURCE_FILES += \
+	$(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
+	$(TARGET_OUT_SHARED_LIBRARIES)/libicuuc.so \
+	$(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
+	$(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so
+
+PRODUCT_COPY_FILES += \
+	$(OUT_DIR)/target/product/Nord/obj/SHARED_LIBRARIES/libandroidicu_intermediates/libandroidicu.so:$(TARGET_COPY_OUT_RECOVERY)/root/system/lib64/libandroidicu.so
+
 # QCOM Decryption
 PRODUCT_PACKAGES += \
     qcom_decrypt \
